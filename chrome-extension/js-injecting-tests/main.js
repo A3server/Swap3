17c0af3cbf8764c26facb5f4f718aba4369462e7
Polpedu <emailparaoedu@gmail.com>
2022-05-21 06:10:13 +0100

    REACT push

--- chrome-extension/js-injecting-tests/main.js
+++ chrome-extension/js-injecting-tests/main.js
@@ -1,23 +1,74 @@
 
 
+/*
+const config = {
+    networkId: "mainnet",
+    keyStore,
+    nodeUrl: "https://rpc.mainnet.near.org",
+    walletUrl: "https://wallet.mainnet.near.org",
+    helperUrl: "https://helper.mainnet.near.org",
+    explorerUrl: "https://explorer.mainnet.near.org",
+};
+  */
+
+function httpGET(url) {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onload = function () {
+            if (this.status == 200) {
+                console.log(this.response);
+                resolve(this.response);
+            }
+            else {
+                var error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+        xhr.onerror = function () {
+            reject(new Error("Network Error"));
+        };
+        xhr.send();
+    });
+}
+
 $(document).ready(function() {   // Load the function after DOM ready.
-    // grab    
     console.log("A3wap is running");
+    const nearhttpdefault= "https://wallet.near.org/"
+    
 
+    /*const borshSerialize = { kind: 'struct', fields: [
+        ['signerId', 'string'],
+        ['publicKey', PublicKey],
+        ['nonce', 'u64'],
+        ['receiverId', 'string'],
+        ['blockHash', [32]],
+        ['actions', [Action]]
+    ]};*/
+
+
+    
     // grab transactions from the url search params
-    var transactions = getUrlVars()["transactions"];
+    var transactionsBytesbase64 = getUrlVars()["transactions"];
+    console.log(transactionsBytesbase64)
+    //parse transactions bytes in base64 in the curretn TransactionSchema model
+    var transactions = Transaction.fromBase64(transactionsBytesbase64);
+    // parse transactions string from base64 to string without using atob
+    const transactionwierdstruct = window.atob(transactions);
+    // transactions is a byte array, convert to string
+    console.log(ASCIIfromArray)
 
-    // convert transactions from base64 to text
-    const ASCIItransacitons = atob(transactions);
 
-    console.log(ASCIItransacitons);
+    console.log(transactionwierdstruct);
 
     //! this is scuffed, but it works
     // get the string in between the brakets of ASCII transactions string 
-    var args = ASCIItransacitons.substring(ASCIItransacitons.indexOf("{\"t"), ASCIItransacitons.indexOf("\"}")+2);
+    var args = ASCIItransacitons.substring(ASCIItransacitons.indexOf("{\""), ASCIItransacitons.indexOf("\"}")+2);
     console.log(args);
     //parse args to json
     var json = JSON.parse(args);
+    console.log("json args");
     console.log(json);
 
     // search for ".near" strings in the ASCII transactions string
@@ -29,8 +80,30 @@ $(document).ready(function() {   // Load the function after DOM ready.
         console.log(users);
     }
 
+    //create transaction object args
+    var transaction = ["edup.near", ]
+    console.log("transaction object");
+    console.log(transaction);
+
+    //parse transaction object to base64
+    var transactionbase64 = btoa(JSON.stringify(transaction));
+
+
+
+    // create a new url to push a transaction
+    const url = nearhttpdefault + "sign?transactions=" + transactionbase64;
+
+
+    httpGET('https://indexer.ref-finance.net/list-token-price').then(function(response) {
+        console.log(response);
+    });
+
+    //redirect to the new url
+    //window.location.href = url;
+
 
 });
+
 
 function getUrlVars() {
     var vars = {};
--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -1,30 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import axios from 'axios';
 import './App.css';
+import * as nearAPI from "near-api-js";
+import { Tonic } from '@tonic-foundation/tonic';
+
+import { Buffer } from 'buffer';
+import { Transaction } from 'near-api-js/lib/transaction';
+import HomePage from './screens/Homepage';
+import About from './screens/About';
+import Settings from './screens/Settings';
+
+const transaction = "CQAAAGVkdXAubmVhcgDw/EipmYzmqGhQLkms72qqOh4VzqqZDGyeza42hhpPDsL+URrpOwAAFgAAAG1hcmtldHBsYWNlLnBhcmFzLm5lYXLthHDVKMqu1UraTXZTWU82OA2kJfAv6RdLOi39//GDaAEAAAACAwAAAGJ1eXQAAAB7InRva2VuX2lkIjoiMzIyIiwibmZ0X2NvbnRyYWN0X2lkIjoibWludC5oYXZlbmRhby5uZWFyIiwiZnRfdG9rZW5faWQiOiJuZWFyIiwicHJpY2UiOiIxNjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAifQBgt5hsiAAAAAAAoIQUQGFRWYQAAAAAAA==";
+const CONTRACT_ACCOUNT = 'v1.orderbook.testnet';
+const tonic = new Tonic(account, CONTRACT_ACCOUNT);
+
+async function getAllTonicMarkets() {
+
+}
+
 
 function App() {
 
+  const [prices, setPrices] = React.useState([]);
+  const [err, setErr] = React.useState(null);
+
   React.useEffect(() => {
-    console.log('hello');
+
+    // get the url vars
+    const urlParams = new URLSearchParams(window.location.search);
+    const transactions = urlParams.get('transactions');
+    console.log(transactions);
+
+
+    //base 64 hex to transaction serialization
+    const transactionDecoded = Transaction.decode(Buffer.from(transaction, 'base64'))
+    console.log(transactionDecoded)
+
+    // the transactionDecoded.
+    const TxsArgs = transactionDecoded.actions[0].functionCall.args;
+    
+    
+    //parse txsArgs from bytearray to string
+    const TxsArgsString = JSON.parse(Buffer.from(TxsArgs).toString())
+    console.log("Public Key:")
+    console.log(TxsArgsString)
+
+    const UserpublicKey = TxsArgsString.publicKey;
+    const signerId = TxsArgsString.signerId;
+
+
+
+
+    //? FOR main net: make a get request to ref fiance
+    /*axios.get('https://indexer.ref-finance.net/list-token-price').then(function (response) {
+      console.log(response);
+      setPrices(response.data);
+    }).catch(function (error) {
+      setErr(error);
+      console.log(error);
+    });*/
+
+
+    // for tonic testnet
+    const account = await near.account(signerId);
+    
+
+
+
+
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route exact path="/" element={<HomePage/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/settings" element={<Settings/>}/>
+      </Routes>
+    </Router>
   );
 }
 
