a6b80af4f855f6b2256a00299bdf8866a911747a
ertem0 <35893360+ertem0@users.noreply.github.com>
2022-05-22 08:54:08 -0400

    contract final

--- DEX/contracts/src/lib.rs
+++ DEX/contracts/src/lib.rs
@@ -16,6 +16,8 @@ use near_sdk::json_types::ValidAccountId;
 pub trait ExtSelf {
     fn add_token_resolve(token_addr:AccountId) ->bool;
     fn swap_resolve(&mut self, v:Value);
+    fn deposit_resolve(&mut self, v:Value);
+
 }
 
 #[ext_contract(ext_dex_calls)]
@@ -76,13 +78,10 @@ impl Contract {
     
 
 
-    pub fn ft_on_transfer(&mut self, sender_id:AccountId, amount:String, msg:String)-> Option<String> {
+    pub fn ft_on_transfer(&mut self, sender_id:AccountId, amount:String, msg:String)-> String {
         let token_address = env::predecessor_account_id();
-        let v: Value = serde_json::from_str(&msg).ok()?;
-        //let swap_args = v["swap_args"];
-        //let target_args = v["target_args"];
-        let a = "1035000000000000000000000";
-        env::log(format!("attached_amount: {} {}",v["target_args"]["contract"].to_string().trim_matches('"'), a).as_bytes());
+        //let v: Value = serde_json::from_str(&msg).ok()?;
+
 
         env::log(format!("TOKEN ADDR: {}, PREPAID_GAS {:?}", token_address, env::prepaid_gas()).as_bytes());
         ext_dex_calls::ft_transfer_call(
@@ -91,22 +90,18 @@ impl Contract {
             "".to_string(), 
             None,
             token_address,
-            1,
-            Gas(30000000000000)
-        ).and(
-        Promise::new(self.dex_contract.clone()).function_call(
-            "swap".to_string(),
-            json!(v["swap_args"]).to_string().as_bytes().to_vec(),
-            1,
-            Gas(50000000000000)
-        )).then(
-            ext_self::swap_resolve(
-                v,
-                env::current_account_id(),
-                0,
-                Gas(100000000000000)
-            )
+            1,  
+            Gas(10000000000000)
         );
+        // .then(
+        //     ext_self::deposit_resolve(
+        //         v,
+        //         env::current_account_id(),
+        //         0,
+        //         Gas(10000000000000)
+        //     )
+        // );
+        
         // ).and(
         //     ext_dex_calls::place_bid(
         //         v["market_id"].as_str().unwrap().to_string(),
@@ -117,13 +112,43 @@ impl Contract {
                 
         //     )
         // );
-        Some("0".to_string())
+        return "ola".to_string();
+    }
+
+    #[private]
+    pub fn deposit_resolve(&mut self, v:Value){
+        assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
+        match env::promise_result(0) {
+        PromiseResult::NotReady => unreachable!(),
+        PromiseResult::Successful(val) => {
+            if let Ok(amount) = near_sdk::serde_json::from_slice::<String>(&val) {
+                
+                Promise::new(self.dex_contract.clone()).function_call(
+                    "swap".to_string(),
+                    json!(v["swap_args"]).to_string().as_bytes().to_vec(),
+                    1,
+                    Gas(10000000000000)
+                ).then(
+                    ext_self::swap_resolve(
+                        v,
+                        env::current_account_id(),
+                        0,
+                        Gas(50000000000000)
+                    )
+                );
+
+            } else {
+                env::panic(b"ERR_WRONG_VAL_RECEIVED")
+            }
+        },
+        PromiseResult::Failed => env::panic(b"ERR_CALL_FAILED"),
+        }
     }
 
     #[private]
     pub fn swap_resolve(&mut self, v:Value) {
-        assert_eq!(env::promise_results_count(), 2, "ERR_TOO_MANY_RESULTS");
-        match env::promise_result(1) {
+        assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
+        match env::promise_result(0) {
         PromiseResult::NotReady => unreachable!(),
         PromiseResult::Successful(val) => {
             if let Ok(amount) = near_sdk::serde_json::from_slice::<String>(&val) {
@@ -151,11 +176,8 @@ impl Contract {
     }
     }
 
-    pub fn test(&self) -> Option<Vec<AccountId>> {
-        let data = "{\"force\":0,\"actions\":[{\"pool_id\":2999,\"token_in\":\"kongztoken.near\",\"token_out\":\"wrap.near\",\"amount_in\":\"57000000000\",\"min_amount_out\":\"724258232698179300000000\"}]}";
-        let v: Value = serde_json::from_str(data).ok()?;
-        env::log(format!("Please call {} at the number {}", v["force"], v["actions"][0]).as_bytes());
-        return Some(self.supported_tokens.clone());
+    pub fn test(&self) -> AccountId {
+        return self.dex_contract.clone();
     }
 
 
